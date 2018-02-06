a5df7753b743e85eb7346b1a95000b7ed306ba25
Anthony Chu <antchu@microsoft.com>
2018-02-06 14:48:31 -0800

    Initial commit

---
+++ src/cdatracker.js
@@ -0,0 +1,52 @@
+function cdaTracker(config) {
+  var defaultDomains = [
+    /(.*\.)?microsoft\.com$/,
+    /(.*\.)?msdn\.com$/,
+    /(.*\.)?visualstudio\.com$/,
+    "www.microsoftevents.com"
+  ];
+
+  var domains = config.domains;
+  if (domains || Array.isArray(domains)) {
+    domains = domains.concat(defaultDomains);
+  } else {
+    domains = defaultDomains;
+  }
+
+  config.domains = domains;
+
+  appendTrackingInfo(config, [].slice.call(document.getElementsByTagName('a')));
+}
+
+function appendTrackingInfo(config, links) {
+  var needsTracking = 
+    links.filter(l => domainNeedsTracking(l, config.domains) && !isAlreadyTracked(l));
+
+  for (var l of needsTracking) {
+    var tracking = 'WT.mc_id=' + config.campaign + '-' + config.medium + '-' + config.alias;
+    if (l.search) {
+      l.search = l.search + '&' + tracking;
+    } else {
+      l.search = '?' + tracking;
+    }
+  }
+}
+
+function isAlreadyTracked(link) {
+  return /[\?\&]WT\.mc_id=/i.test(link.search);
+}
+
+function domainNeedsTracking(link, domains) {
+  return domains.some(d => {
+    if (d instanceof RegExp) {
+      return d.test(link.hostname);
+    } else if (typeof(d) === 'string') {
+      return d === link.hostname;
+    }
+    return false;
+  });
+}
+
+if (typeof(module) !== 'undefined' && module.exports) {
+  module.exports.appendTrackingInfo = appendTrackingInfo;
+}
